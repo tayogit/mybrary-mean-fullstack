888facee12065370a32330370b40d6f916a7821c
Adesiyan <tayonetu80@gmail.com>
2019-06-23 18:23:23 +0100

    initial author route

--- server.js
+++ server.js
@@ -4,17 +4,20 @@ require("./models/db");
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine','ejs')
 app.set('views',__dirname + '/views')
 app.set('layout', 'layouts/layout')
+
 app.use(expressLayouts)
 app.use(express.static('public'))
-
-
-
+app.use(bodyParser.urlencoded({limit:'10mb',extended:false}))
 
 app.use('/',indexRouter)
+app.use('/authors',authorRouter)
+
 app.listen(process.env.PORT || 3000)
